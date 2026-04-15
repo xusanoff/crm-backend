b370fbfe425b3d69c6905affa77685f25cf728af
xusanoff <xusanoff.abdurahmon@gmail.com>
2026-04-15 16:54:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors         import CORS
 from flask_jwt_extended import JWTManager
 from models             import db, migrate, bcrypt
 from utils.utils        import create_admin
+from sshtunnel          import SSHTunnelForwarder
 
 from routes.auth           import auth_bp
 from routes.admin          import admin_bp
@@ -14,26 +15,27 @@ from routes.lesson_cancel  import lesson_bp
 from routes.expenses       import expense_bp
 from routes.teacher        import teacher_bp, group_salary_bp
 
+# ── SSH TUNNEL ────────────────────────────────────────────────────────────────
+tunnel = SSHTunnelForwarder(
+    ('185.180.231.43', 22),
+    ssh_username='root',
+    ssh_password='TCjTvI6Y02VM',
+    remote_bind_address=('127.0.0.1', 5432)
+)
+tunnel.start()
+
+DB_URL = f"postgresql://akbarov:akbarov@127.0.0.1:{tunnel.local_bind_port}/my_zone_crm_jizzax_db"
+
 
 def create_app():
     app = Flask(__name__)
 
     # ── CORS ──────────────────────────────────────────────────────────────────
-    # CORS(app,
-    #      origins=["https://my-zone-crm-frontend.vercel.app"],
-    #      methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    #      allow_headers=["Content-Type", "Authorization"],
-    #      supports_credentials=True)
     CORS(app)
 
     # ── DATABASE ──────────────────────────────────────────────────────────────
-   
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-        "DATABASE_URL", "postgresql://postgres:2006@127.0.0.1:5432/crm_db"
-    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    
 
     # ── JWT ───────────────────────────────────────────────────────────────────
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "h7v8yebfyvo37vo8vc8p4")
